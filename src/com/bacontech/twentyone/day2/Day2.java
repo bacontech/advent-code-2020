87c57a9a769a94cce4edb6e822ccf73a68cdc5f7
Alex Bacon <fosizzle@gmail.com>
2021-12-02 08:40:08 -0600

    day2 21

--- src/com/bacontech/twentyone/day2/Day2.java
+++ src/com/bacontech/twentyone/day2/Day2.java
@@ -7,14 +7,15 @@ import java.util.List;
 public class Day2 extends Answer {
     public static void main(String[] args) {
 
-        String input = SAMPLE;
+        // SAMPLE vs FULL_PROMPT
+        String input = FULL_PROMPT;
 
-        Object part1 = solvePart1(input);
-        printSolution("part1", part1);
+//        Object part1 = solvePart1(input);
+//        printSolution("part1", part1);
 
 
-        // Object part2 = solvePart2(input);
-        // printSolution("part2", part2);
+         Object part2 = solvePart2(input);
+         printSolution("part2", part2);
     }
 
     public static Object solvePart1(String filename) {
@@ -33,11 +34,58 @@ public class Day2 extends Answer {
     }
 
     public long processAnswerPart1(List<String> lines) {
-        return 0L;
+        long horizontalPos = 0L;
+        long depthPos = 0L;
+
+        for(String command : lines) {
+            String[] parts = command.split(" ");
+            String direction = parts[0];
+            long units = Long.parseLong(parts[1]);
+
+            if ("forward".equals(direction)) {
+                horizontalPos += units;
+            }
+            if ("down".equals(direction)) {
+                depthPos += units;
+            }
+            if ("up".equals(direction)) {
+                depthPos -= units;
+            }
+        }
+
+        System.out.println(horizontalPos);
+        System.out.println(depthPos);
+        return horizontalPos * depthPos;
     }
 
     public long processAnswerPart2(List<String> lines) {
-        return 0L;
+        long horizontalPos = 0L;
+        long depthPos = 0L;
+        long aim = 0L;
+
+        for(String command : lines) {
+            String[] parts = command.split(" ");
+            String direction = parts[0];
+            long units = Long.parseLong(parts[1]);
+
+            if ("down".equals(direction)) {
+                aim += units;
+            }
+            if ("up".equals(direction)) {
+                aim -= units;
+            }
+
+            if ("forward".equals(direction)) {
+                horizontalPos += units;
+                long deltaDepth = aim * units;
+                depthPos += deltaDepth;
+            }
+        }
+
+        System.out.println(horizontalPos);
+        System.out.println(depthPos);
+        System.out.println(aim);
+        return horizontalPos * depthPos;
     }
 
 
